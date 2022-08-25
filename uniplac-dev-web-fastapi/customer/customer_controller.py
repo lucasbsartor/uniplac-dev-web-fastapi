b5648b987ca352dac173a875f50eb68d41733603
lbonasartor <lbonasartor@gmail.com>
2022-08-24 21:16:52 -0300

    Added simple tests and placeholder dtos

--- uniplac-dev-web-fastapi/customer/customer_controller.py
+++ uniplac-dev-web-fastapi/customer/customer_controller.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+class Customer(BaseModel):
+    name: str
+    cpf: str
+    phone: str
+    password: str
+    registration: str
+    group: str
 
 
 @router.get('/customers', tags=["customers"])
@@ -10,14 +20,19 @@ async def get_customers():
 
 @router.get('/customers/{id}', tags=["customers"])
 async def get_customer(id: int):
-    return {"msg": "Hello from get customer"}
+    return {"msg": "Hello from get customer", "id": id}
 
 
 @router.post('/customers', tags=["customers"])
-async def create_customer():
-    return {"msg": "Hello from post customers"}
+async def create_customer(customer: Customer):
+    return {"msg": "Hello from post customers", "customer": customer}
 
 
 @router.put('/customers/{id}', tags=["customers"])
-async def update_customer(id: int):
-    return {"msg": "Hello from put customers"}
+async def update_customer(id: int, customer: Customer):
+    return {"msg": "Hello from put customers", "id": id, "customer": customer}
+
+
+@router.delete('/customers/{id}', tags=["customers"])
+async def get_customer(id: int):
+    return {"msg": "Hello from delete customer", "id": id}
--- uniplac-dev-web-fastapi/employee/employee_controller.py
+++ uniplac-dev-web-fastapi/employee/employee_controller.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+class Employee(BaseModel):
+    name: str
+    cpf: str
+    phone: str
+    password: str
+    registration: str
+    group: str
 
 
 @router.get('/employees', tags=["employees"])
@@ -10,14 +20,19 @@ async def get_employees():
 
 @router.get('/employees/{id}', tags=["employees"])
 async def get_employee(id: int):
-    return {"msg": "Hello from get employee"}
+    return {"msg": "Hello from get employee", "id": id}
 
 
 @router.post('/employees', tags=["employees"])
-async def create_employee():
-    return {"msg": "Hello from post employees"}
+async def create_employee(employee: Employee):
+    return {"msg": "Hello from post employees", "employee": employee}
 
 
 @router.put('/employees/{id}', tags=["employees"])
-async def update_employee(id: int):
-    return {"msg": "Hello from put employees"}
+async def update_employee(id: int, employee: Employee):
+    return {"msg": "Hello from put employees", "id": id, "employee": employee}
+
+
+@router.delete('/employees/{id}', tags=["employees"])
+async def get_employee(id: int):
+    return {"msg": "Hello from delete employee", "id": id}
